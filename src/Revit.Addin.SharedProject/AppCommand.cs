9b4475bc856d5c36a5de068d60afde6c04411b39
Cesare-Caoduro_aecom <cesare.caoduro@aecom.com>
2022-07-25 14:40:12 +1000

    first commit

---
+++ src/Revit.Addin.SharedProject/AppCommand.cs
@@ -0,0 +1,37 @@
+﻿using Autodesk.Revit.UI;
+using Revit.Addin.SharedProject.Buttons;
+using System;
+using System.Linq;
+
+namespace Revit.Addin.SharedProject
+{
+    public class AppCommand : IExternalApplication
+    {
+        string tabName = "TabName";
+        string ribbonPanelName = "RibbonPanelName";
+
+        public Result OnStartup(UIControlledApplication application)
+        {
+            try
+            {
+                application.CreateRibbonTab(tabName);
+            }
+            catch (Exception)
+            {
+                // Ignored
+            }
+
+            var ribbonPanel = application.GetRibbonPanels().FirstOrDefault(x => x.Name == ribbonPanelName)
+                ?? application.CreateRibbonPanel(tabName, ribbonPanelName);
+
+            FirstButtonCommand.CreateButton(ribbonPanel);
+
+            return Result.Succeeded;
+        }
+
+        public Result OnShutdown(UIControlledApplication application)
+        {
+            return Result.Succeeded;
+        }
+    }
+}
---
+++ src/Revit.Addin.SharedProject/Buttons/FirstButtonCommand.cs
@@ -0,0 +1,46 @@
+﻿using Autodesk.Revit.DB;
+using Autodesk.Revit.UI;
+using System;
+using System.Collections.Generic;
+using System.Reflection;
+using System.Text;
+using System.Windows;
+
+namespace Revit.Addin.SharedProject.Buttons
+{
+    public class FirstButtonCommand : IExternalCommand
+    {
+        private static string buttonText = $"First\nButton";
+        private static string buttonTooltip = $"First Button Tooltip";
+
+        public Result Execute(ExternalCommandData commandData, ref string message, ElementSet elements)
+        {
+            try
+            {
+                MessageBox.Show("First Button Commnad", "AECOM Tech", MessageBoxButton.OK);
+                return Result.Succeeded;
+            }
+            catch (Exception)
+            {
+
+                return Result.Failed;
+            }
+        }
+
+        internal static void CreateButton(RibbonPanel ribbonPanel)
+        {
+            var assembly = Assembly.GetExecutingAssembly();
+            ribbonPanel.AddItem(new PushButtonData(
+                MethodBase.GetCurrentMethod().DeclaringType?.Name,
+                buttonText,
+                assembly.Location,
+                MethodBase.GetCurrentMethod().DeclaringType?.FullName
+                )
+            {
+                ToolTip = buttonTooltip,
+            }
+            );
+        }
+
+    }
+}
