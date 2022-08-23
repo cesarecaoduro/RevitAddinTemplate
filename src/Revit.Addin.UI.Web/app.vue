a7f561b9957eabf12cd27fd2aea27e13ab6a2293
Cesare Caoduro <cesare.caoduro@gmail.com>
2022-08-23 16:35:37 +1000

    tested typescript interface

--- src/Revit.Addin.UI.Web/app.vue
+++ src/Revit.Addin.UI.Web/app.vue
@@ -1,5 +1,16 @@
+<script setup lang="ts">
+interface IUser {
+  name: string;
+  email: string;
+}
+
+const user: IUser = {
+  name: "Cesare",
+  email: "cesare.caoduro@aecom.com",
+};
+
+console.log(user);
+</script>
 <template>
-  <div>
-    <NuxtWelcome />
-  </div>
+  <div>Hello from Nuxt</div>
 </template>
